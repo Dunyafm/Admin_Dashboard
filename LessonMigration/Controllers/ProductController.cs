1daf5a297a9e3475d5f63fa420bbd3dcd733d8df
Dunyafm <dunyafm@code.edu.az>
2022-03-14 17:16:45 -0700

    Adminpanel creat

---
+++ LessonMigration/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using LessonMigration.Data;
+using LessonMigration.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMigration.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Product> products = await _context.Products.Where(p => p.IsDeleted == false)
+               .Include(m => m.Category)
+               .Include(m => m.Images)
+               .OrderByDescending(m => m.Id)
+               .Take(8)
+               .ToListAsync();
+
+            return View(products);
+        }
+
+        public IActionResult LoadMore()
+        {
+            List<Product> products = _context.Products.Where(p => p.IsDeleted == false)
+              .OrderByDescending(m => m.Id)
+              .Skip(4)
+              .Take(4)
+              .ToList();
+
+            return Json(products);
+        }
+    }
+}
